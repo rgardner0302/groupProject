7cba8951f336252368d638dcfa6c7235ffb158c8
Zoe Dyksman <zbybee@hotmail.com>
2017-08-12 07:04:01 -0600

    google sign in

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -38,19 +38,51 @@ if (currentUser != null) {
   $("#signout").html('<li><button class="button" data-open="registerModal">Sign Out</button></li>');
 }
 
-//google sign in
-$('.login').on('click', function() {
-  event.preventDefault();
-  var clientId = '478137109323-67523o1tnhd0ol9bndqpvf4ojm1kk1i1.apps.googleusercontent.com';
-  var apiKey = 'AIzaSyDBAdHjSdgGLClbiEGBdS2Xcp6Yx27WJPk';
-  var queryURL = 'https://www.googleapis.com/auth/calendar';
-  
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-    .done(function(response) {
+//GOOGLE SIGN IN
 
-      var googleLogin = response.data
+//get user information
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  // The ID token you need to pass to your backend:
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token);
+  };
+
+//google sign out
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+    console.log('User signed out.');
     });
+  }
+
+//add event reminder to calendar
+$('.remind').on('click', function() {
+  var clientId = '478137109323-67523o1tnhd0ol9bndqpvf4ojm1kk1i1.apps.googleusercontent.com';
+  //use browser api key
+  var apiKey = 'AIzaSyDBAdHjSdgGLClbiEGBdS2Xcp6Yx27WJPk';
+  var scope = 'https://www.googleapis.com/auth/calendar';
+
+  //insert calendar event
+		var event = {
+      date: $(".date-input").val().trim(),
+  		remember:$(".remember-input").val().trim()
+  	}
+		console.log(event.date);
+		console.log(event.remember);
+		console.log(gapi.client);
+	  var request = gapi.client.calendar.events.insert({
+	  'calendarId': 'primary',
+	  'resource': event
+	  });
+
+	request.execute(function(event) {
+	  appendPre('Event created: ' + event.htmlLink);
+	});
+	  	
 });
+   
